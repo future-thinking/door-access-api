e93880b0aafefc1fea6da92579139dfa20a3ef2c
Shatter00 <jschanstra10@gmail.com>
2021-03-17 18:01:06 +0100

    add /open endpoint

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import express from 'express'
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import path from 'path'
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 import TokenGenerator from 'uuid-token-generator'
+import bearerToken from 'express-bearer-token'
+
 
 const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
 
@@ -15,47 +17,59 @@ var db;
 (async () => {
     // open the database
     db = await open({
-      filename: path.join(__dirname, '../api.db'),
-      driver: sqlite3.Database,
-      
+        filename: path.join(__dirname, '../api.db'),
+        driver: sqlite3.Database,
+
     })
     db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)");
     db.run("CREATE TABLE IF NOT EXISTS tokens (user TEXT, token TEXT)");
     db.run("CREATE TABLE IF NOT EXISTS cards (user TEXT, card TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS permissions (user TEXT, permission TEXT)");
 })()
 
 const app = express()
 
 app.use(express.json())
 
+app.use(bearerToken());
 
+const doorModes = {
+    DEFAULT: "default",
+    SCAN: "scan",
+    OPEN: "open"
+}
 
-app.get("/user/:user/cards", async (req, res)=> {
-    req.params.user
+var doorMode = doorModes.DEFAULT
 
-    const user = await db.get("SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)", [req.params.user]);
+app.get("/door", async (req, res) => {
+    //todo door authentication
 
-    console.log(user);
+    const cardData = req.query.card;
 
-    if (!user){
-        res.send("user not found")
+    const card = await db.get("SELECT *, rowid FROM cards WHERE card=?", [cardData]);
+
+    console.log(card);
+
+    if (!card) {
+        res.send(JSON.stringify({
+            open: false,
+            reason: "card not found"
+        }))
         return;
     }
 
-    const cards = await db.all("SELECT * FROM cards WHERE user=?", [user.rowid])
 
-    console.log(user.rowid);
+    const permission = await db.get("SELECT *, rowid FROM permissions WHERE permission=\"door.open\" AND user=?", [card.user]);
 
-    res.send(cards);
-})
+    console.log(permission);
 
-app.get("/info", (req, res) => {
-    res.send(JSON.stringify(
-        {
-            version: 1,
-            api: "ft"
-        }
-    ))
+    if (!permission) {
+        res.send(JSON.stringify({
+            open: false,
+            reason: "no permission"
+        }))
+        return;
+    }
 })
 
 app.get('/auth', async (req, res) => {
@@ -86,8 +100,89 @@ app.get('/auth', async (req, res) => {
 })
 
 
+app.use(async (req, res, next) => {
 
-app.listen(80, function (){
+
+    if (!req.token) {
+        res.send("unauthorized");
+        return;
+    }
+
+    const user = await db.get("SELECT *, rowid FROM tokens WHERE token=?", [req.token]);
+
+    if (!user) {
+        res.send("unauthorized")
+        return;
+    }
+
+    const permissions = await db.all("SELECT *, rowid FROM permissions WHERE user=?", [user.user]) ?? [];
+    req.permissions = permissions
+
+    const userObject = await db.get("SELECT *, rowid FROM users WHERE rowid=?", [user.user]);
+    req.user = userObject
+
+    next()
+});
+
+app.get("/user/:user/cards", async (req, res) => {
+    req.params.user
+
+    const user = await db.get("SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)", [req.params.user]);
+
+    console.log(user);
+
+    if (!user) {
+        res.send("user not found")
+        return;
+    }
+
+    const cards = await db.all("SELECT *, rowid FROM cards WHERE user=?", [user.rowid])
+
+    console.log(user.rowid);
+
+    res.send(cards);
+})
+
+app.get("/info", (req, res) => {
+    res.send(JSON.stringify(
+        {
+            version: 1,
+            api: "ft"
+        }
+    ))
+})
+
+app.put("/open", (req, res) => {
+
+    if (req.permissions.includes("open_door")) {
+
+        doorMode = doorModes.OPEN
+        setTimeout(() => {
+            doorMode = doorModes.DEFAULT
+        },
+
+            10000
+
+        )
+
+        res.send("ok")
+    }
+
+    else {
+        res.send("insufficient permission")
+    }
+
+
+})
+
+app.get("/cards", (req, res) => {
+    
+    res.redirect(`/user/${req.user.username}/cards`)
+
+})
+
+
+app.listen(80, function () {
     console.log('App listening')
-} )
+})
 
