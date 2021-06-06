653ea390f59250f20fcb649ad8c20989dd6bb1b9
RobinD95 <lopfi95@gmail.com>
2021-06-06 17:07:31 +0200

    minor changes

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ import { fileURLToPath } from 'url'
 import TokenGenerator from 'uuid-token-generator'
 import bearerToken from 'express-bearer-token'
 
-
 const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// this is a top-level await 
-var db;
+let db;
+
 (async () => {
     // open the database
     db = await open({
@@ -21,10 +20,11 @@ var db;
         driver: sqlite3.Database,
 
     })
-    db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)");
-    db.run("CREATE TABLE IF NOT EXISTS tokens (user TEXT, token TEXT)");
-    db.run("CREATE TABLE IF NOT EXISTS cards (user TEXT, card TEXT)");
-    db.run("CREATE TABLE IF NOT EXISTS permissions (user TEXT, permission TEXT)");
+    await db.run(`CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)`);
+    await db.run(`CREATE TABLE IF NOT EXISTS tokens (user TEXT, token TEXT)`);
+    await db.run(`CREATE TABLE IF NOT EXISTS cards (user TEXT, card TEXT)`);
+    await db.run(`CREATE TABLE IF NOT EXISTS permissions (user TEXT, permission TEXT)`);
+    console.log("Initialized Database");
 })()
 
 const app = express()
@@ -39,7 +39,7 @@ const doorModes = {
     OPEN: "open"
 }
 
-var doorMode = {
+let doorMode = {
     mode: doorModes.DEFAULT
 }
 
@@ -54,21 +54,15 @@ app.post("/add/user", async (req, res) => {
         return;
     }
 
-    const existing = await db.get("SELECT username FROM users WHERE LOWER(username)=LOWER(?)", [username]);
+    const existing = await db.get(`SELECT username FROM users WHERE LOWER(username)=LOWER(?)`, [username]);
     
     if (!existing) {
-        const user = await db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, password]);
+        await db.run(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, password]);
     
         res.status(200).send("ok");
-
-        return;
     }
 
-    else {
-        res.send("user already exists")
-
-        return;
-    }
+    else res.send("user already exists");
 });
 
 app.get("/door", async (req, res) => {
@@ -76,17 +70,17 @@ app.get("/door", async (req, res) => {
 
     const cardData = req.query.card;
 
-    const card = await db.get("SELECT *, rowid FROM cards WHERE card=?", [cardData]);
+    const card = await db.get(`SELECT *, rowid FROM cards WHERE card=?`, [cardData]);
 
     console.log(card);
 
-    if (doorMode.mode == doorModes.SCAN) {
+    if (doorMode.mode === doorModes.SCAN) {
         doorMode.mode = doorModes.DEFAULT
 
-        const existing = await db.get("SELECT card FROM cards WHERE card=?", [cardData]);
+        const existing = await db.get(`SELECT card FROM cards WHERE card=?`, [cardData]);
 
         if (!existing) {        
-            db.run("INSERT INTO cards (user, card) VALUES (?, ?)", [doorMode.user.rowid, cardData])
+            db.run(`INSERT INTO cards (user, card) VALUES (?, ?)`, [doorMode.user.rowid, cardData])
     
             res.send(JSON.stringify({
                 open: false,
@@ -102,11 +96,8 @@ app.get("/door", async (req, res) => {
                 reason: "scanning",
                 card: "card already exists"
             }))
-
             return;
         }
-
-        return;
     }
 
     if (!card) {
@@ -117,21 +108,19 @@ app.get("/door", async (req, res) => {
         return;
     }
 
-    const permission = await db.get("SELECT *, rowid FROM permissions WHERE permission=\"open.door\" AND user=?", [card.user]);
+    const permission = await db.get(`SELECT *, rowid FROM permissions WHERE permission='open.door' AND user=?`, [card.user]);
 
     if (!permission) {
         res.send(JSON.stringify({
             open: false,
             reason: "no permission"
         }))
-        return;
     }
     else {
         res.send(JSON.stringify({
             open: true,
             reason: "allowed"
         }))
-        return;
     }
 })
 
@@ -140,7 +129,7 @@ app.get('/auth', async (req, res) => {
 
         const { username, password } = req.body;
 
-        const result = await db.get("SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?) AND password=?", [username, password]);
+        const result = await db.get(`SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?) AND password=?`, [username, password]);
 
         if (!result) {
             res.status(405).send("incorrect username or password");
@@ -151,14 +140,12 @@ app.get('/auth', async (req, res) => {
 
         const token = tokgen.generate()
 
-        db.run("INSERT INTO tokens VALUES (?, ?)", [user, token]);
+        db.run(`INSERT INTO tokens VALUES (?, ?)`, [user, token]);
 
         res.send(token);
 
-    } else {
-        res.status(400).send("no username and password")
-    }
-})
+    } else res.status(400).send("no username and password");
+});
 
 
 app.use(async (req, res, next) => {
@@ -169,40 +156,37 @@ app.use(async (req, res, next) => {
         return;
     }
 
-    const user = await db.get("SELECT *, rowid FROM tokens WHERE token=?", [req.token]);
+    const user = await db.get(`SELECT *, rowid FROM tokens WHERE token=?`, [req.token]);
 
     if (!user) {
-        res.send("unauthorized")
+        res.send("unauthorized");
         return;
     }
 
-    const permissions = (await db.all("SELECT *, rowid FROM permissions WHERE user=?", [user.user])) .map(el => el.permission);
-    req.permissions = permissions
+    req.permissions = (await db.all(`SELECT *, rowid FROM permissions WHERE user=?`, [user.user])) .map(el => el.permission);
 
-    const userObject = await db.get("SELECT *, rowid FROM users WHERE rowid=?", [user.user]);
-    req.user = userObject
+    req.user = await db.get(`SELECT *, rowid FROM users WHERE rowid=?`, [user.user]);
 
     next()
 });
 
 app.get("/user/:user/cards", async (req, res) => {
-    req.params.user
 
-    const user = await db.get("SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)", [req.params.user]);
+    const user = await db.get(`SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)`, [req.params.user]);
 
     if (!user) {
-        res.send("user not found")
+        res.send("user not found");
         return;
     }
 
-    const cards = await db.all("SELECT *, rowid FROM cards WHERE user=?", [user.rowid])
+    const cards = await db.all(`SELECT *, rowid FROM cards WHERE user=?`, [user.rowid]);
 
     res.send(cards);
 })
 
 app.post("/add/:user/card", async (req, res) => {
 
-    const userID = await db.get("SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)", [req.params.user]);
+    const userID = await db.get(`SELECT *, rowid FROM users WHERE LOWER(username) LIKE LOWER(?)`, [req.params.user]);
 
     if (req.permissions.includes("add.card")) {
 
@@ -212,57 +196,38 @@ app.post("/add/:user/card", async (req, res) => {
         }
         
         setTimeout(() => {
-            
             doorMode = { 
                 mode: doorModes.DEFAULT
             }
-        },
-
-            10000
-
-        )
+        },10000)
 
         res.status(200).send("ok");
     }
 
-    else {
-        res.send("insufficient permission");
-    }
+    else res.send("insufficient permission");
 })
 
 app.patch("/user/:user/permissions/:permission", async (req, res) => {
     
-    const permission = req.params.permission
-    const username = await db.get("SELECT *, rowId FROM users WHERE LOWER(username)=LOWER(?)", [req.params.user]);
-    const existing = await db.get("SELECT permission FROM permissions WHERE permission=? AND user=?", [permission, username.rowid]);
+    const permission = req.params.permission;
+    const username = await db.get(`SELECT *, rowId FROM users WHERE LOWER(username)=LOWER(?)`, [req.params.user]);
+    const existing = await db.get(`SELECT permission FROM permissions WHERE permission=? AND user=?`, [permission, username.rowid]);
 
-    if (req.permissions.includes("add.permission") && req.query.changePermission == "add" && !existing){
-        await db.all("INSERT INTO permissions (user, permission) VALUES (?, ?)", [username.rowid, permission])
-
-        res.status(200).send("ok");
-
-        return;
-    }
-
-    else if (req.permissions.includes("remove.permission") && req.query.changePermission == "remove"){
-        await db.all("DELETE FROM permissions WHERE user=? AND permission=?", [username.rowid, permission])
+    if (req.permissions.includes("add.permission") && req.query.changePermission === "add" && !existing){
+        await db.all("INSERT INTO permissions (user, permission) VALUES (?, ?)", [username.rowid, permission]);
 
         res.status(200).send("ok");
-
-        return;
     }
 
-    else if (existing){
-        res.send("permission already exists")
-        
-        return;
+    else if (req.permissions.includes("remove.permission") && req.query.changePermission === "remove"){
+        await db.all("DELETE FROM permissions WHERE user=? AND permission=?", [username.rowid, permission]);
+
+        res.status(200).send("ok");
     }
 
-    else {
-        res.send("insufficient permission")
+    else if (existing)res.send("permission already exists");
 
-        return;
-    }
+    else res.send("insufficient permission");
 }); 
 
 app.get("/info", (req, res) => {
@@ -271,8 +236,8 @@ app.get("/info", (req, res) => {
             version: 1,
             api: "ft"
         }
-    ))
-})
+    ));
+});
 
 app.put("/open", async (req, res) => {
 
@@ -281,31 +246,23 @@ app.put("/open", async (req, res) => {
         doorMode = doorModes.OPEN
         setTimeout(() => {
             doorMode = doorModes.DEFAULT
-        },
-
-            10000
-
-        )
+        },10000);
 
         res.send("ok");
     }
 
-    else {
-        res.send("insufficient permission")
-    }
-
-
-})
+    else res.send("insufficient permission");
+});
 
 app.get("/cards", (req, res) => {
     
-    res.redirect(`/user/${req.user.username}/cards`)
+    res.redirect(`/user/${req.user.username}/cards`);
 
-})
+});
 
 
 app.listen(80, function () {
-    console.log('App listening')
-})
+    console.log('App listening');
+});
 
 
